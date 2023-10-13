6ed686955e16870148ebf9ab3346d3b9a43050c5
Marco Castro <marcodotcastro@gmail.com>
2023-10-13 14:32:59 -0300

    build: config flowbite

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,14 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+    content: [
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/views/**/*',
+        './node_modules/flowbite/**/*.js'
+    ],
+    theme: {
+        extend: {},
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
+    plugins: [
+        require('flowbite/plugin')
+    ]
 }
