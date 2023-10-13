44d184b928d51caf7c13cda7fac668638c36a990
Marco Castro <marcodotcastro@gmail.com>
2023-10-13 16:00:14 -0300

    feat: sample index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def convert_to_id(resource)
+    resource.to_s.parameterize.concat("-" + resource.id.to_s)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+
+  def to_s
+    title
+  end
 end
