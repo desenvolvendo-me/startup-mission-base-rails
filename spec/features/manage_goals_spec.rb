6c2394ce17592b650544f36b13bbaeedcdb6d5f3
Philipe Rodrigues <dev.philipe@outlook.com>
2023-10-17 15:09:46 -0300

    test(i18n Goals): tests working

--- spec/features/manage_goals_spec.rb
+++ spec/features/manage_goals_spec.rb
@@ -3,38 +3,38 @@ require 'rails_helper'
 RSpec.feature "Manager Goals", type: :feature do
 
   before do
-    Goal.create!(name: "Learn Ruby")
-    Goal.create!(name: "Learn Rails")
+    Goal.create!(name: "Aprender Ruby")
+    Goal.create!(name: "Aprender Rails")
   end
 
   scenario "User visits the goals index page" do
     visit manager_goals_path
 
-    expect(page).to have_text("Learn Ruby")
-    expect(page).to have_text("Learn Rails")
+    expect(page).to have_text("Aprender Ruby")
+    expect(page).to have_text("Aprender Rails")
   end
 
   scenario "User creates a new goal" do
     visit new_manager_goal_path
 
-    fill_in "Name", with: "Learn Ruby on Rails"
-    click_button "Save"
+    fill_in "Nome", with: "Aprender Ruby on Rails"
+    click_button "Salvar"
 
-    expect(page).to have_text("Goal was successfully created.")
-    expect(page).to have_text("Learn Ruby on Rails")
+    expect(page).to have_text("Objetivo cadastrado com sucesso.")
+    expect(page).to have_text("Aprender Ruby on Rails")
   end
 
   scenario "User views a goal" do
     visit manager_goal_path(Goal.first)
 
-    expect(page).to have_text("Learn Ruby")
+    expect(page).to have_text("Aprender Ruby")
   end
 
   scenario "User deletes a goal" do
     visit manager_goal_path(Goal.first)
-    click_link "Destroy"
+    click_link I18n.t('views.manager.goals.delete')
 
-    expect(page).to have_text("Goal was successfully destroyed.")
-    expect(page).not_to have_text("Learn Ruby")
+    expect(page).to have_text("Objetivo apagado com sucesso.")
+    expect(page).not_to have_text("Aprender Ruby")
   end
 end
