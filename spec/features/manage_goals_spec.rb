e6497d09559fd4a5ebe7ab6475beb90acebf5695
Marco Castro <marcodotcastro@gmail.com>
2023-10-19 11:56:18 -0300

    refactor: lint

--- spec/features/manage_goals_spec.rb
+++ spec/features/manage_goals_spec.rb
@@ -2,31 +2,31 @@ require 'rails_helper'
 
 RSpec.feature 'Manager Goals', type: :feature do
   before do
-    Goal.create!(name: "Aprender Ruby")
-    Goal.create!(name: "Aprender Rails")
+    Goal.create!(name: 'Aprender Ruby')
+    Goal.create!(name: 'Aprender Rails')
   end
 
   scenario 'User visits the goals index page' do
     visit manager_goals_path
 
-    expect(page).to have_text("Aprender Ruby")
-    expect(page).to have_text("Aprender Rails")
+    expect(page).to have_text('Aprender Ruby')
+    expect(page).to have_text('Aprender Rails')
   end
 
   scenario 'User creates a new goal' do
     visit new_manager_goal_path
 
-    fill_in "Nome", with: "Aprender Ruby on Rails"
-    click_button "Salvar"
+    fill_in 'Nome', with: 'Aprender Ruby on Rails'
+    click_button 'Salvar'
 
-    expect(page).to have_text("Objetivo cadastrado com sucesso.")
-    expect(page).to have_text("Aprender Ruby on Rails")
+    expect(page).to have_text('Objetivo cadastrado com sucesso.')
+    expect(page).to have_text('Aprender Ruby on Rails')
   end
 
   scenario 'User views a goal' do
     visit manager_goal_path(Goal.first)
 
-    expect(page).to have_text("Aprender Ruby")
+    expect(page).to have_text('Aprender Ruby')
   end
 
   scenario 'User deletes a goal' do
@@ -34,7 +34,7 @@ RSpec.feature 'Manager Goals', type: :feature do
     click_link I18n.t('views.manager.goals.delete')
 
     page.accept_alert I18n.t('views.manager.goals.delete_confirm')
-    expect(page).to have_text("Objetivo apagado com sucesso.")
-    expect(page).not_to have_text("Aprender Ruby")
+    expect(page).to have_text('Objetivo apagado com sucesso.')
+    expect(page).not_to have_text('Aprender Ruby')
   end
 end
