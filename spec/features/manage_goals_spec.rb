686cb01bc33e98b7d5bbc7729ba2c373e8cbc716
Marco Castro <marcodotcastro@gmail.com>
2023-10-19 11:56:00 -0300

    fix: test

--- spec/features/manage_goals_spec.rb
+++ spec/features/manage_goals_spec.rb
@@ -32,8 +32,8 @@ RSpec.feature 'Manager Goals', type: :feature do
   scenario 'User deletes a goal' do
     visit manager_goal_path(Goal.first)
     click_link I18n.t('views.manager.goals.delete')
-    
-    page.accept_alert 'Are you sure?'
+
+    page.accept_alert I18n.t('views.manager.goals.delete_confirm')
     expect(page).to have_text("Objetivo apagado com sucesso.")
     expect(page).not_to have_text("Aprender Ruby")
   end
