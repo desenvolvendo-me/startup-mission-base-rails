29b2c509463f8f0cf96e4e7711104ac50e32260c
Marco Castro <marcodotcastro@gmail.com>
2023-10-20 12:22:32 -0300

    chore: admin i18n

--- app/admin/goals.rb
+++ app/admin/goals.rb
@@ -1,36 +1,51 @@
 ActiveAdmin.register Goal do
-  menu parent: 'Gestão'
-  permit_params :name, :description,
+  menu parent: I18n.t('active_admin.menu.management')
+  permit_params :name, :description, :status,
                 tasks_attributes: %i[id name status description _destroy]
+
+  index do
+    id_column
+    column :goal
+    column :name
+    column :description
+    column :status do |task|
+      status_tag I18n.t("enum.statuses.#{task.status}"), class: task.status.to_s
+    end
+    actions
+  end
 
   show do
     attributes_table do
       row :name
       row :description
+      row :status do |task|
+        status_tag I18n.t("enum.statuses.#{task.status}"), class: task.status.to_s
+      end
     end
 
-    panel 'Tarefas' do
+    panel I18n.t("active_admin.goal.tasks") do
       table_for goal.tasks do
         column :name
         column :description
         column :status do |task|
-          status_tag task.status, class: task.status.to_s
+          status_tag I18n.t("enum.statuses.#{task.status}"), class: task.status.to_s
         end
       end
     end
   end
 
   form do |f|
-    f.inputs 'Meta' do
+    f.inputs I18n.t('activerecord.models.goal.one') do
       f.input :name
       f.input :description
+      f.input :status, as: :select, collection: Goal.statuses.keys.map { |key| [I18n.t("enum.statuses.#{key}"), key] }
     end
 
-    f.inputs 'Detalhes das Tarefas' do
-      f.has_many :tasks, allow_destroy: true, heading: 'Tarefas' do |task|
+    f.inputs I18n.t('active_admin.goal.task.nested') do
+      f.has_many :tasks, allow_destroy: true, heading: I18n.t('active_admin.goal.tasks') do |task|
         task.input :name
         task.input :description
-        task.input :status, as: :select, collection: Task.statuses.keys
+        task.input :status, as: :select, collection: Task.statuses.keys.map { |key| [I18n.t("enum.statuses.#{key}"), key] }
       end
     end
 
