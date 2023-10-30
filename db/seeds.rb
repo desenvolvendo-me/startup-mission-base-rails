453f7594a9054bcd758aaeb2c7131085ae417470
Philipe <dev.philipe@outlook.com>
2023-10-30 17:41:09 -0300

    chore(seeds): create more seeds to see pagination working

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,70 @@ if Rails.env.development?
                       description: 'Quero criar 5 projetos simples em até 3 meses', status: 'todo')
   Task.create(name: '1ª projeto', description: 'Criar a editora de livros',
               status: "todo", goal: goal2)
+
+  goal3 = Goal.create(name: 'Aprender Linguagem Python',
+                      description: 'Quero criar 5 scripts úteis em até 2 meses', status: 'doing')
+  Task.create(name: '1º script', description: 'Criar um script de automação de tarefas',
+              status: "doing", goal: goal3)
+
+  goal4 = Goal.create(name: 'Aprender Banco de Dados SQL',
+                      description: 'Quero criar um banco de dados de livros em até 1 mês', status: 'todo')
+  Task.create(name: '1ª tabela', description: 'Criar a tabela de livros',
+              status: "todo", goal: goal4)
+
+  goal5 = Goal.create(name: 'Aprender Front-End Development',
+                      description: 'Quero construir um portfólio online em 2 semanas', status: 'doing')
+  Task.create(name: 'Página inicial', description: 'Criar a página inicial do meu portfólio',
+              status: "doing", goal: goal5)
+
+  goal6 = Goal.create(name: 'Aprender Linguagem JavaScript',
+                      description: 'Quero dominar os conceitos básicos em 1 mês', status: 'todo')
+  Task.create(name: '1º exercício', description: 'Realizar um exercício de lógica em JavaScript',
+              status: "todo", goal: goal6)
+
+  goal7 = Goal.create(name: 'Aprender Desenvolvimento Mobile',
+                      description: 'Quero criar um aplicativo simples em 2 meses', status: 'doing')
+  Task.create(name: 'Protótipo de tela', description: 'Desenhar o protótipo da tela inicial do aplicativo',
+              status: "doing", goal: goal7)
+
+  goal8 = Goal.create(name: 'Aprender Testes de Software',
+                      description: 'Quero escrever testes para um projeto em 1 mês', status: 'todo')
+  Task.create(name: 'Configurar ambiente', description: 'Configurar o ambiente de testes no projeto',
+              status: "todo", goal: goal8)
+
+  goal9 = Goal.create(name: 'Aprender Cloud Computing',
+                      description: 'Quero implantar um aplicativo em nuvem em 2 semanas', status: 'doing')
+  Task.create(name: 'Configurar servidor', description: 'Configurar um servidor na nuvem',
+              status: "doing", goal: goal9)
+
+  goal10 = Goal.create(name: 'Aprender Design de Interface',
+                       description: 'Quero criar um design para um site em 1 mês', status: 'todo')
+  Task.create(name: 'Layout da página inicial', description: 'Criar o layout da página inicial do site',
+              status: "todo", goal: goal10)
+
+  goal11 = Goal.create(name: 'Aprender Machine Learning',
+                       description: 'Quero criar um modelo de machine learning em 2 meses', status: 'doing')
+  Task.create(name: 'Coleta de dados', description: 'Coletar dados para treinar o modelo',
+              status: "doing", goal: goal11)
+
+  goal12 = Goal.create(name: 'Aprender Redes de Computadores',
+                       description: 'Quero configurar uma rede local em 3 semanas', status: 'todo')
+  Task.create(name: 'Configurar roteadores', description: 'Configurar os roteadores da rede',
+              status: "todo", goal: goal12)
+
+  goal13 = Goal.create(name: 'Aprender Segurança da Informação',
+                       description: 'Quero realizar um teste de penetração em 1 mês', status: 'doing')
+  Task.create(name: 'Varredura de vulnerabilidades', description: 'Realizar uma varredura de vulnerabilidades no sistema',
+              status: "doing", goal: goal13)
+
+  goal14 = Goal.create(name: 'Aprender Desenvolvimento Web Full-Stack',
+                       description: 'Quero criar um aplicativo completo em 2 meses', status: 'doing')
+  Task.create(name: 'Desenvolvimento do backend', description: 'Iniciar o desenvolvimento do backend do aplicativo',
+              status: "doing", goal: goal14)
+
+  goal15 = Goal.create(name: 'Aprender Inteligência Artificial',
+                       description: 'Quero criar um chatbot em 1 mês', status: 'todo')
+  Task.create(name: 'Treinamento do modelo', description: 'Treinar um modelo de chatbot',
+              status: "todo", goal: goal15)
+
 end
