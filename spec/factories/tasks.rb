861c30ca3af6bc09a2405aa1182d42f438f55cc2
Marco Castro <marcodotcastro@gmail.com>
2023-11-01 10:43:50 -0300

    build: annotation config

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :bigint           not null, primary key
+#  description :string
+#  finished_at :datetime
+#  name        :string
+#  status      :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  goal_id     :bigint
+#
+# Indexes
+#
+#  index_tasks_on_goal_id  (goal_id)
+#
 FactoryBot.define do
   factory :task do
     name { '1ª agoritmo' }
