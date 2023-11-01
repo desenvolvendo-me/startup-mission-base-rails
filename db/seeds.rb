6a7d655a3fa7d12c4d717db6e237e4b677175c53
Marco Castro <marcodotcastro@gmail.com>
2023-11-01 10:29:17 -0300

    feat: dashboard goal and task

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,16 @@ if Rails.env.development?
   AdminUser.create!(email: 'admin@mail.com',
                     password: 'password', password_confirmation: 'password')
   goal1 = Goal.create(name: 'Aprender Linguagem Ruby',
-                      description: 'Quero criar 10 algoritmos em até 3 meses', status: 'done')
+                      description: 'Quero criar 10 algoritmos em até 3 meses',
+                      status: 'done', finished_at: Date.today - 5.days)
   Task.create(name: '1ª algoritmo', description: 'Criar o algoritmo bubble sort',
-              status: "done", goal: goal1)
+              status: "done", goal: goal1, finished_at: Date.today - 6.days)
 
   goal2 = Goal.create(name: 'Aprender Framework Rails',
-                      description: 'Quero criar 5 projetos simples em até 3 meses', status: 'done')
+                      description: 'Quero criar 5 projetos simples em até 3 meses',
+                      status: 'done', finished_at: Date.today)
   Task.create(name: '1ª projeto', description: 'Criar a editora de livros',
-              status: "done", goal: goal2)
+              status: "done", goal: goal2, finished_at: Date.today)
 
   goal3 = Goal.create(name: 'Aprender Linguagem Python',
                       description: 'Quero criar 5 scripts úteis em até 2 meses', status: 'doing')
