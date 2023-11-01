e981de7e42d959d76549668fcd4a46f157d0d1ce
Marco Castro <marcodotcastro@gmail.com>
2023-11-01 10:28:53 -0300

    test: change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,14 @@ if Rails.env.development?
   AdminUser.create!(email: 'admin@mail.com',
                     password: 'password', password_confirmation: 'password')
   goal1 = Goal.create(name: 'Aprender Linguagem Ruby',
-                      description: 'Quero criar 10 algoritmos em até 3 meses', status: 'doing')
+                      description: 'Quero criar 10 algoritmos em até 3 meses', status: 'done')
   Task.create(name: '1ª algoritmo', description: 'Criar o algoritmo bubble sort',
-              status: "doing", goal: goal1)
+              status: "done", goal: goal1)
 
   goal2 = Goal.create(name: 'Aprender Framework Rails',
-                      description: 'Quero criar 5 projetos simples em até 3 meses', status: 'todo')
+                      description: 'Quero criar 5 projetos simples em até 3 meses', status: 'done')
   Task.create(name: '1ª projeto', description: 'Criar a editora de livros',
-              status: "todo", goal: goal2)
+              status: "done", goal: goal2)
 
   goal3 = Goal.create(name: 'Aprender Linguagem Python',
                       description: 'Quero criar 5 scripts úteis em até 2 meses', status: 'doing')
