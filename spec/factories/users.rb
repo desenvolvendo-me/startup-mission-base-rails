71beae3a187925004b6043a89f63e851d70cfa97
eskokado <eskokado@gmail.com>
2024-04-22 09:49:15 -0300

    feat: add fields trackable

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,14 +7,19 @@
 #  confirmation_sent_at   :datetime
 #  confirmation_token     :string
 #  confirmed_at           :datetime
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  first_name             :string
 #  last_name              :string
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
 #  name                   :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
 #  unconfirmed_email      :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
