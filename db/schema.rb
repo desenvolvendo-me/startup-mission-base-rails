4fd6840981b726e96bc13b779e8ad56f8798408d
eskokado <eskokado@gmail.com>
2024-04-22 08:17:16 -0300

    feat(users): add fields by confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -113,12 +113,17 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_17_082407) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "first_name"
     t.string "last_name"
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
