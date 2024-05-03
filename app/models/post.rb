459e504cb36a84f102af9df37fa3c7aae42d4118
eskokado <eskokado@gmail.com>
2024-05-03 14:23:16 -0300

    fix: comment posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Post < ApplicationRecord
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
