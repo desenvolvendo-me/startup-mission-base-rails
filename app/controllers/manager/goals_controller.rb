276012cef1361569810e576352d560700c2efc47
Marco Peixoto <marcoaspeixoto@gmail.com>
2024-05-04 23:11:28 -0300

    feat: changing search and filter

--- app/controllers/manager/goals_controller.rb
+++ app/controllers/manager/goals_controller.rb
@@ -1,5 +1,12 @@
 module Manager
   class GoalsController < InternalController
+
+    def index
+      @q = Goal.ransack(params[:q])
+      @goals = @q.result(distinct: true)
+      @goals = @goals.order('created_at').page(params[:page]).per(4)
+    end
+
     private
 
     def goal_params
